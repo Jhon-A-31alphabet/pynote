14a3b5fbac426a873d011bf0cd939154db13fffe
Jhon Marcos <kenzhait@gmail.com>
2022-11-12 14:05:15 -0800

    ultima funcionalidad personalizacion de fuente

---
+++ fuctions.py
@@ -0,0 +1,54 @@
+
+
+from tkinter import *
+from tkinter import filedialog
+from tkinter import font
+from tkinter.colorchooser import askcolor
+
+
+
+
+def guardar(x):
+    a = filedialog.asksaveasfile(defaultextension=".txt",filetypes=[("textfile",".txt"),("pdf files",".pdf")])
+    b = f"{a}.txt"
+    file_Text =x.get(1.0,END)
+    a.write(file_Text)
+    a.close()
+
+
+
+def oscuro(x,y):
+    ventana = y.config(bg="gray")
+    caja_texto = x.config(bg ="black",fg="white")
+    
+    barra_texto = x.config(insertbackground = "white")
+   
+
+def modo_claro(x,y):
+    ventana = y.config(bg="white")
+    caja_texto = x.config(bg ="white",fg="black")
+    
+    barra_texto = x.config(insertbackground = "black")
+
+
+def personalizar_fondo(x,y):
+    fondo_color =askcolor(title="color de fondo")
+    caja_Texto_color =askcolor(title="caja de texto")
+    fuente_caja_texto = askcolor(title="fuente")
+
+    x.configure(bg=fondo_color[1])
+    y.configure(bg=caja_Texto_color[1])
+    y.configure(fg=fuente_caja_texto[1])
+
+
+
+    #----------------
+
+
+
+
+   
+
+   
+
+
---
+++ interface.py
@@ -0,0 +1,41 @@
+from tkinter import *
+
+from fuctions import *
+
+
+
+
+class ui:
+    def __init__(self,master):
+        self.caja_texto = Text(master,borderwidth=5,padx=10,pady=0,font=60)
+       
+        self.caja_texto.config(font=('Arial', 16))
+        self.caja_texto.place(x=0,y=30)
+
+        self.boton_guardar = Button(master,text="Guardar",command = lambda:guardar(self.caja_texto))
+        self.boton_guardar.place(x=10,y=1)
+
+        self.boton_personalizar = Button(master,text="Personalizar fondo",command=lambda:personalizar_fondo(master,self.caja_texto))
+        self.boton_personalizar.place(x=60,y=1)
+
+        self.boton_oscuro = Button(master,text="Noche",command=lambda :oscuro(self.caja_texto,master))
+        self.boton_oscuro.place(x=130,y=1)
+
+        self.boton_claro =Button(master,text = "claro",command=lambda:modo_claro(self.caja_texto,master))
+        self.boton_claro.place(x=170,y=1)
+
+        
+#-----------------------------------------------------------------------------------------instancias---------------------
+
+
+try:
+    root = Tk()
+    root.title("Pynotepad")
+    root.config(bg="white")
+    root.geometry("1080x700")
+    ar = ui(root)
+except:
+    root.showerror("invalido","hubo un error")
+
+finally:
+    root.mainloop()
